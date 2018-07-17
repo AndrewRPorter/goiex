0067db5d93ff9617be622e91b7417b1c249d9036
AndrewRPorter <pavelow53.andrew@gmail.com>
2018-07-17 14:49:09 -0400

    added acessors for values

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
    "fmt"
    "log"
+   "net/http"
    "encoding/json"
-   "github.com/levigross/grequests"
 )
 
 type Stock struct{
@@ -12,36 +12,17 @@ type Stock struct{
    CompanyName string
    PrimaryExchange string
    Sector string
-   CalculationPrice string
    Open float64
    OpenTime int
    Close float64
    CloseTime int
    High float64
    Low float64
-   LatestPrice float64
-   LatestSource string
-   LatestTime string
-   LatestUpdate int
    LatestVolume int
    IexRealtimePrice float64
-   IexRealtimeSize int
-   IexLastUpdated int
-   DelayedPrice float64
-   DelayedPriceTime int
-   ExtendedPrice float64
-   ExtendedChange float64
-   ExtendedChangePercent float64
-   ExtendedPriceTime int
    Change float64
    ChangePercent float64
-   IexMarketPercent float64
-   IexVolume int
    AvgTotalVolume int
-   IexBidPrice float64
-   IexBidSize int
-   IexAskPrice float64
-   IexAskSize int
    MarketCap int
    PeRatio float64
    Week52High float64
@@ -49,17 +30,17 @@ type Stock struct{
    YtdChange float64
 }
 
-func fetch(ticker string) Stock {
+func get(ticker string) Stock {
    url := fmt.Sprintf("https://api.iextrading.com/1.0/stock/%s/quote", ticker)
-   resp, err := grequests.Get(url, nil)
+   resp, err := http.Get(url)
 
    if err != nil {
-    log.Fatalln("Unable to make request: ", err)
+      log.Fatalln("Unable to make request: ", err)
    }
 
    s := Stock{}
 
-   err = json.NewDecoder(resp).Decode(&s)
+   err = json.NewDecoder(resp.Body).Decode(&s)
 
    if err != nil {
       log.Fatalln("Unable to decode JSON: ", err)
@@ -68,17 +49,81 @@ func fetch(ticker string) Stock {
    return s
 }
 
-func (s *Stock) get_price() float64 {
-   return s.LatestPrice
-}
-
 func (s *Stock) get_symbol() string {
    return s.Symbol
 }
 
-func main() {
-   stock := fetch("GEVO")
+func (s *Stock) get_company_name() string {
+   return s.CompanyName
+}
 
-   fmt.Println(stock.get_symbol())
-   fmt.Println(stock.get_price())
+func (s *Stock) get_primary_exchange() string {
+   return s.PrimaryExchange
+}
+
+func (s *Stock) get_sector() string {
+   return s.Sector
+}
+
+func (s *Stock) get_open() float64 {
+   return s.Open
+}
+
+func (s *Stock) get_open_time() int {
+   return s.OpenTime
+}
+
+func (s *Stock) get_close() float64 {
+   return s.Close
+}
+
+func (s *Stock) get_close_time() int {
+   return s.CloseTime
+}
+
+func (s *Stock) get_volume() int {
+   return s.LatestVolume
+}
+
+func (s *Stock) get_price() float64 {
+   return s.IexRealtimePrice
+}
+
+func (s *Stock) get_change() float64 {
+   return s.Change
+}
+
+func (s *Stock) get_percent_change() float64 {
+   return s.ChangePercent
+}
+
+func (s *Stock) get_average_volume() int {
+   return s.AvgTotalVolume
+}
+
+func (s *Stock) get_market_cap() int {
+   return s.MarketCap
+}
+
+func (s *Stock) get_ratio() float64 {
+   return s.PeRatio
+}
+
+func (s *Stock) get_52_high() float64 {
+   return s.Week52High
+}
+
+func (s *Stock) get_52_low() float64 {
+   return s.Week52Low
+}
+
+func (s *Stock) get_year_change() float64 {
+   return s.YtdChange
+}
+
+func main() {
+   s := get("GEVO")
+
+   fmt.Println(s.get_symbol())
+   fmt.Println(s.get_price())
 }
